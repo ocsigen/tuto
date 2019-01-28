17381049060455caf9daa6ae14d20a0264e389d9
Vincent Balat <vincent.balat@vblt.org>
2019-01-28 16:16:54 +0100

    Fix latest

--- latest/manual/.travis-ocaml.sh
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head><meta http-equiv="refresh" content="0; URL=../../2.6/manual/./.travis-ocaml.sh" /></head>
-    <body></body>
-</html>
--- latest/manual/how.sh
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head><meta http-equiv="refresh" content="0; URL=../../2.6/manual/./how.sh" /></head>
-    <body></body>
-</html>

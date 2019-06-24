53e7014748e54becd5e955eb9f49d0648e617f8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-24 15:12:14 +0000

    Deploy ocsigen/tuto to github.com/ocsigen/tuto.git:gh-pages

--- latest/manual/files/tutorial/chapter3/multigraffiti_macaque/create_table.sql
+++ latest/manual/files/tutorial/chapter3/multigraffiti_macaque/create_table.sql
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head><meta http-equiv="refresh" content="0; URL=../../6.3/./manual/files/tutorial/chapter3/multigraffiti_macaque/create_table.sql" /></head>
+    <head><meta http-equiv="refresh" content="0; URL=../../6.3/manual/files/tutorial/chapter3/multigraffiti_macaque/create_table.sql" /></head>
     <body></body>
 </html>
--- latest/manual/files/tutorial/chapter3/multigraffiti_ocsipersist/server.ml
+++ latest/manual/files/tutorial/chapter3/multigraffiti_ocsipersist/server.ml
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head><meta http-equiv="refresh" content="0; URL=../../6.3/./manual/files/tutorial/chapter3/multigraffiti_ocsipersist/server.ml" /></head>
+    <head><meta http-equiv="refresh" content="0; URL=../../6.3/manual/files/tutorial/chapter3/multigraffiti_ocsipersist/server.ml" /></head>
     <body></body>
 </html>
